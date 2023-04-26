fd195c8659db7310baa13ae54401a4badb3c5408
Isaac Vieira <isaacvf.dev@gmail.com>
2023-04-25 22:04:32 -0300

    Funções em config, modicação na formatação em env.txt e modificação em funções no utils.py

--- pages/config.py
+++ pages/config.py
@@ -2,17 +2,30 @@ import PySimpleGUI as sg
 import datetime
 import configparser
 
-current_year = datetime.datetime.now().year
-previous_year = current_year - 1
+def select_year():
+    current_year = datetime.datetime.now().year
+    previous_year = current_year - 1
+    return [str(current_year), str(previous_year)]
+
+
+def returnMonth(month=None):
+    months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+    if month is None:
+        return months
+    elif month in months:
+        return str(months.index(month) + 1)
+    else:
+        return ""
+
 
 layout = [
     [sg.Text("Dados Pessoais")],
     [sg.Text("CNPJ:"), sg.Input(size=(14, 1), key="-CNPJ-", enable_events=True)],
     [sg.Text("Gerar DAS")],
-    [sg.Text("Mês:"), sg.Combo(['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'], key="-MONTH-", enable_events=True)],
-    [sg.Text("Ano:"), sg.Combo([str(current_year), str(previous_year)], default_value=str(current_year), size=(4), key="-YEAR-")],
+    [sg.Text("Mês:"), sg.Combo(returnMonth(), key="-MONTH-", enable_events=True)],
+    [sg.Text("Ano:"), sg.Combo(select_year(), size=(4, 1), key="-YEAR-")],
     [sg.Checkbox('Auto', default=False, key="-AUTO-", enable_events=True)],
-    [sg.Button("Salvar")],
+    [sg.Button("Salvar", key="-SAVE-", disabled=True)],
 ]
 
 window = sg.Window("Configurações", layout, size=(250, 215))
@@ -25,30 +38,31 @@ while True:
     if event == sg.WIN_CLOSED:
         break
     if event == "-CNPJ-":
+        if len(values["-CNPJ-"]) == 14:
+            window["-SAVE-"].update(disabled=False)
         print(values["-CNPJ-"])
         if len(values["-CNPJ-"]) > 14:
             values["-CNPJ-"] = values["-CNPJ-"][:14]
             window["-CNPJ-"].update(value=values["-CNPJ-"])
-            sg.popup('A entrada deve ter exatamente 14 caracteres.')
+            sg.popup('CNPJ só pode ter no máximo 14 caracteres')
     if event == "-YEAR-":
         print("-YEAR-")
     if event == "-AUTO-":
         if values["-AUTO-"]:
-            print('Clicado')
             window["-MONTH-"].update('')
             window["-YEAR-"].update('')
-            window["-MONTH-"].update(disabled=True)
-            window["-YEAR-"].update(disabled=True)
-        else:
-            window["-MONTH-"].update(disabled=False)
-            window["-YEAR-"].update(disabled=False)
-    if event == "Salvar":
+        window["-MONTH-"].update(disabled=values["-AUTO-"])
+        window["-YEAR-"].update(disabled=values["-AUTO-"])
+    if event == "-SAVE-":
         print("Configurando")
         config.set('ENV', 'CNPJ', values['-CNPJ-'])
-        # config.set('MONTH=', 'MONTH', values['-MONTH-'])
+        config.set('ENV', 'MONTH', returnMonth(values['-MONTH-']))
         config.set('ENV', 'YEAR', values['-YEAR-'])
-        # config.set('SEÇÃO', 'AUTO', values['-AUTO-'])
+        config.set('ENV', 'AUTO', str(values['-AUTO-']))
         with open('../env.txt', 'w') as configfile:
             config.write(configfile)
-
+            print(configfile)
+        with open('../env.txt', 'r') as configfile:
+            data = configfile.read()
+            print(data)
 window.close()
